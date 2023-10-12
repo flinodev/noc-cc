67a9387ea15502e50cb5b9498a1ea412b2006aa7
Francisco Lino <franksuriel_7@hotmail.com>
2023-10-12 15:03:04 -0600

    ADD cron service at presentation layer

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,11 @@
-import { CronJob } from "cron";
+import { CronService } from "./cron/cron-service";
 
 export class Server {
   public static start() {
     console.log("Server started, now!");
+    CronService.createJob("*/10 * * * * *", () => {
+      const date = new Date();
+      console.log("10 seconds ", date);
+    });
   }
 }
