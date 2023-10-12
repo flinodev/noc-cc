d9e57c7e110340120abc43d248a76203e0bd4dd5
Frank712 <franksuriel_7@hotmail.com>
2023-10-12 16:40:55 -0500

    ADD log entity

---
+++ src/domain/entities/log.entity.ts
@@ -0,0 +1,17 @@
+export enum LogSeverityLevel {
+  low = "low",
+  medium = "medium",
+  high = "high",
+}
+
+export class LogEntity {
+  public level: LogSeverityLevel;
+  public message: string;
+  public createdAt: Date;
+
+  constructor(message: string, level: LogSeverityLevel) {
+    this.message = message;
+    this.level = level;
+    this.createdAt = new Date();
+  }
+}
