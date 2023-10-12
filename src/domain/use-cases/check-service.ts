1be3b7f36479041546657f46b0d43615199b8350
Francisco Lino <franksuriel_7@hotmail.com>
2023-10-12 15:27:33 -0600

    ADD DI into check-service

--- src/domain/use-cases/check-service.ts
+++ src/domain/use-cases/check-service.ts
@@ -2,18 +2,24 @@ export interface CheckServiceUserCase {
   execute(url: string): Promise<boolean>;
 }
 
+type SuccessCallback = () => void;
+type ErrorCallback = (error: string) => void;
+
 export class CheckService implements CheckServiceUserCase {
-  constructor() {}
+  constructor(
+    private readonly successCallback: SuccessCallback,
+    private readonly errorCallback: ErrorCallback
+  ) {}
   async execute(url: string): Promise<boolean> {
     try {
       const req = await fetch(url);
       if (!req.ok) {
         throw new Error(`Error on check service ${url}`);
       }
-      console.log(`${url} is ok!`);
+      this.successCallback();
       return true;
     } catch (error) {
-      console.log(`${error}`);
+      this.errorCallback(`${error}`);
       return false;
     }
   }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -4,9 +4,12 @@ import { CronService } from "./cron/cron-service";
 export class Server {
   public static start() {
     console.log("Server started, now!");
-    CronService.createJob("*/10 * * * * *", () => {
-      const date = new Date();
-      new CheckService().execute("https:google.com");
+    CronService.createJob("*/5 * * * * *", () => {
+      const url = "https://google.com";
+      new CheckService(
+        () => console.log(`${url} is ok!`),
+        (error) => console.log(error)
+      ).execute(url);
     });
   }
 }
