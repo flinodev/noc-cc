7cf28330baa4c231d1041feb771e7e4b39af6cca
Frank712 <franksuriel_7@hotmail.com>
2023-10-13 02:19:34 -0500

    ADD email send at server.ts

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,9 @@
+import { envs } from "../config/plugins/envs.plugin";
 import { CheckService } from "../domain/use-cases/check-service";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
+import { EmailService } from "./email/email-service";
 
 const fileSystemLogRepository = new LogRepositoryImpl(
   new FileSystemDatasource()
@@ -9,6 +11,8 @@ const fileSystemLogRepository = new LogRepositoryImpl(
 export class Server {
   public static start() {
     console.log("Server started, now!");
+    // const emailService = new EmailService(fileSystemLogRepository);
+    // emailService.sendEmailWithFileSystemLogs(["franksuriel_7@hotmail.com"]);
     CronService.createJob("*/5 * * * * *", () => {
       const url = "http://localhost:3000/posts";
       new CheckService(
