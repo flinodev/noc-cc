ce86d8bdb4bf65f02db5508cfc163ae54638d5bc
Frank712 <franksuriel_7@hotmail.com>
2023-10-15 04:34:47 -0500

    ADD fromObject function into log entity

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -30,4 +30,15 @@ export class LogEntity {
     log.createdAt = new Date(createdAt);
     return log;
   }
+
+  static fromObject(object: { [key: string]: any }): LogEntity {
+    const { message, level, createdAt, origin } = object;
+    const log = new LogEntity({
+      message,
+      level,
+      createdAt,
+      origin,
+    });
+    return log;
+  }
 }
