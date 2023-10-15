827bd97b09904ffcc2cfe4c751c28e3a79883350
Frank712 <franksuriel_7@hotmail.com>
2023-10-14 23:24:23 -0500

    ADD mongo envirnoment variables

--- src/config/plugins/envs.plugin.ts
+++ src/config/plugins/envs.plugin.ts
@@ -7,4 +7,8 @@ export const envs = {
   MAILER_MAIL: env.get("MAILER_MAIL").required().asEmailString(),
   MAILER_SECRET_KEY: env.get("MAILER_SECRET_KEY").required().asString(),
   PROD: env.get("PROD").required().asBool(),
+  MONGO_URL: env.get("MONGO_URL").required().asString(),
+  MONGO_DB_NAME: env.get("MONGO_DB_NAME").required().asString(),
+  MONGO_USER: env.get("MONGO_USER").required().asString(),
+  MONGO_PASS: env.get("MONGO_PASS").required().asString(),
 };
